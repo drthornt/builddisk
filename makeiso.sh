85484c85ea9340fe85eb4f2f7311bf86b69e445d
David Thornton <northdot9@gmail.com>
2015-05-14 23:50:28 -0400

    added some stuff

---
+++ makeiso.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo mkdir
+
+~/kickstart_build
+~/kickstart_build/isolinux
+~/kickstart_build/isolinux/images
+~/kickstart_build/isolinux/ks
+~/kickstart_build/isolinux/LiveOS
+~/kickstart_build/isolinux/Packages
+~/kickstart_build/utils
+
